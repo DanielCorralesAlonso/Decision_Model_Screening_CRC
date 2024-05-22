a028fa072fd3a75a1f153a55a235cc709987b6c2
DanielCorralesAlonso <danielcorralesalonso@gmail.com>
2024-05-22 09:42:14 +0200

    change values in net from code

---
+++ info_value_to_net.py
@@ -0,0 +1,11 @@
+import pysmile
+import pysmile_license
+import numpy as np
+import pandas as pd
+
+def info_value_to_net(val_scr_data, val_col_data, net):
+
+    net.set_node_definition("Value_of_CRC_detection_by_screening", val_scr_data.flatten())
+    net.set_node_definition("Value_of_CRC_detection_by_colonoscopy", val_col_data.flatten())
+
+    return net
